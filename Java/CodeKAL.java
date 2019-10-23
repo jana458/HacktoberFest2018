924269b2a1e4731aabf96d5c0fa54dda0c67675e
jana458 <56906024+jana458@users.noreply.github.com>
2019-10-23 13:29:44 +0300

    Update CodeKAL.java

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -8,6 +8,7 @@ public class CodeKal {
 		System.out.println("CodeKAL'ı seviyorum!");
 		System.out.println("CodeKAL'ın bir parçası olduğum için çok mutluyum.");
 
-	}
+	}System.out.println("I love CodeKAL!");
+		System.out.println("I'm happy for be a member of CodeKAL.");
 
 }
