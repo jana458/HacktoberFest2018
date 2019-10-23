c7f2d81831d45b80d0a01c12866f7acc0f06dbd4
jana458 <56906024+jana458@users.noreply.github.com>
2019-10-23 19:09:48 +0300

    Update Bölünme.py

--- Python/Bölünme.py
+++ Python/Bölünme.py
@@ -10,3 +10,8 @@ else:
 #GÖREV:Yukarıda verilen örenğin benzerini üçe bölünebilme ile yapınız:
 #!!!Python programlama dilinde boşluklar programın çalışması için önem arz etmektedir.
 #Yukarıdaki örneği baz alarak görevi yerine getirebilirsiniz.
+a = int(input("1.sayı: "))
+if a%3==0:
+   print(a,"sayısı 3 ün katıdır.")
+else:
+   print(a, "sayısı 3 ün katı değildir.")
